0db4abd258d4f3e9bb5b5d152e4dbb50bab04b9d
Nikolay Gofstein <ngofstein@myseneca.ca>
2023-09-12 22:50:47 -0400

    ws1 p1 first attempt

---
+++ WS01/part 1/ws1_p1/RideRequest.cpp
@@ -0,0 +1,129 @@
+/**********************************************************************************************************************************
+				<assessment name: Workshop - #1 (Part-1)>
+Full Name  : Nikolay Gofstein
+Student ID#: 182368217
+Email      : ngofstein@myseneca.ca
+Section    :  ZAA
+Date of Completion: 12-9-23
+
+Authenticity Declaration:
+ I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+***********************************************************************************************************************************/
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include "RideRequest.h"
+
+using namespace sdds;
+using namespace std;
+
+double g_taxrate{};
+double g_discount{};
+
+RideRequest::RideRequest() : m_customerName{}, m_rideDesc{}, m_isDiscounted{}
+{
+}
+
+RideRequest::~RideRequest()
+{
+	setEmpty();
+}
+
+sdds::RideRequest::operator bool() const
+{
+	return !(m_customerName[0] == '\0');
+}
+
+void sdds::RideRequest::setEmpty()
+{
+	m_customerName[0] = '\0';
+}
+
+std::istream& sdds::RideRequest::read(std::istream& input)
+{
+	if (input)//testing if istream is in good state
+	{
+		char tempName[CUSTOMER_NAME_MAX]{};
+		char tempDesc[MAX_RIDE_DESC]{};
+		double tempPrice{};
+		char tempDiscount{};
+		//Read name
+		input.getline(tempName, CUSTOMER_NAME_MAX, ',');
+		//Read description
+		input.getline(tempDesc, MAX_RIDE_DESC, ',');
+		//Read price
+		input >> tempPrice;
+		//ignore delimiter
+		input.get();
+		//Read discount
+		input.get(tempDiscount);
+		//get the newline
+		//input.get();
+
+		//check if reading succeeded
+		if (input)
+		{
+			//Read successful, assign to members
+			strcpy_s(m_customerName, tempName);
+			strcpy_s(m_rideDesc, tempDesc);
+			m_ridePrice = tempPrice;
+			//cout << endl << "HAVE READ PRICE FOR " << m_customerName << " >" << tempPrice << "<" << endl;
+			m_isDiscounted = (tempDiscount == 'Y') ? true : false;
+		}
+		else
+		{
+			//failed reading, destroy this object
+			setEmpty();
+		}
+	}
+	return input;
+}
+
+void sdds::RideRequest::display() const {
+	static size_t count = 1;
+	if (*this)
+	{
+		//calculate tax
+		double priceWithTax = m_ridePrice * (1 + g_taxrate);
+
+		//cout << endl << m_customerName << "'s PRICE IS: >" << m_ridePrice << "<" << endl;
+		//cout << "AFTER TAX: >" << priceWithTax << "<" << endl;
+		//cout << "TAX RATE: >" << g_taxrate << "<" << endl;
+
+		cout << setw(2) << setiosflags(ios::left) << count << ". ";
+		cout << setw(CUSTOMER_NAME_MAX) << m_customerName << '|';
+		cout << setw(MAX_RIDE_DESC) << m_rideDesc << '|';
+		cout << setw(12) << std::fixed << setprecision(2) << priceWithTax << '|' << resetiosflags(ios::left);
+		//if discounted print discounted price
+		if (m_isDiscounted)
+		{
+			double discountedPrice = priceWithTax - g_discount;
+			cout << setiosflags(ios::right) << setw(13) << discountedPrice;
+			cout << resetiosflags(ios::right);
+		}
+		cout << endl;
+	}
+	else
+	{
+		//no customer name
+		cout << setw(2) << count << ". No Ride Request" << endl;
+	}
+	count++;
+}
+
+RideRequest& sdds::RideRequest::operator=(RideRequest& ro)
+{
+	if (ro)
+	{	//if in good state copy all
+		strcpy_s(m_customerName, ro.m_customerName);
+		strcpy_s(m_rideDesc, ro.m_rideDesc);
+		m_ridePrice = ro.m_ridePrice;
+		m_isDiscounted = ro.m_isDiscounted;
+	}
+	else
+	{	//otherwise destroy the object
+		setEmpty();
+	}
+	
+	return *this;
+}
---
+++ WS01/part 1/ws1_p1/RideRequest.h
@@ -0,0 +1,64 @@
+/**********************************************************************************************************************************
+				<assessment name: Workshop - #1 (Part-1)>
+Full Name  : Nikolay Gofstein
+Student ID#: 182368217
+Email      : ngofstein@myseneca.ca
+Section    :  ZAA
+Date of Completion: 12-9-23
+
+Authenticity Declaration:
+ I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+***********************************************************************************************************************************/
+
+#ifndef SDDS_RIDEREQUEST_H
+#define SDDS_RIDEREQUEST_H
+#include <iostream>
+
+extern double g_taxrate;
+extern double g_discount;
+
+const int CUSTOMER_NAME_MAX = 10;
+const int MAX_RIDE_DESC = 25;
+
+namespace sdds
+{
+	class RideRequest
+	{
+		char m_customerName[CUSTOMER_NAME_MAX]{};
+		char m_rideDesc[MAX_RIDE_DESC]{};
+		double m_ridePrice{};
+		bool m_isDiscounted{};
+	public:
+		RideRequest();
+		~RideRequest();
+		/// <summary>
+		/// Read name, desc and boolean if discounted from given istream
+		/// </summary>
+		/// <param name="input">where to read from</param>
+		/// <returns>same istream that was passed</returns>
+		std::istream& read(std::istream& input);
+		/// <summary>
+		/// display counter and contents of the object to cout
+		/// </summary>
+		void display() const;
+		/// <summary>
+		/// copy assignment operator
+		/// </summary>
+		/// <param name="ro">what to assign</param>
+		/// <returns>object after assigning</returns>
+		RideRequest& operator=(RideRequest& ro);
+	private:
+		/// <summary>
+		/// Return true if name populated
+		/// </summary>
+		operator bool()const;
+		/// <summary>
+		/// sets current object to empty state
+		/// </summary>
+		void setEmpty();
+	};
+
+
+}
+
+#endif // !
