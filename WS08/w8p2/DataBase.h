22265400d64cc38b2645e44d13b6bf74378b3651
Nikolay Gofstein <ngofstein@myseneca.ca>
2023-11-17 18:28:07 -0500

    w8p2 first attempt

---
+++ WS08/w8p2/DataBase.h
@@ -0,0 +1,66 @@
+#ifndef SDDS_DATABASE_H
+#define SDDS_DATABASE_H
+// Workshop 8 - Smart Pointers
+
+
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <string>
+#include <memory>
+#include <utility>
+#include <fstream>
+
+namespace sdds {
+    template <typename T>
+    class DataBase {
+		std::vector<T> database;
+	public:
+		DataBase() { }
+		DataBase(const char* fn) {
+			std::ifstream file(fn);
+			if (!file)
+				throw std::string("*** Failed to open file ") + std::string(fn) + std::string(" ***");
+            while (file) {
+                T e;
+				try {
+					if (e.load(file))
+						database.push_back(T(e));
+				}
+				catch (...) {
+					break;
+				}
+            }
+		}
+		size_t size() const { return database.size(); }
+		const T& operator[](size_t i) const { return database[i]; }
+
+		// TODO: Overload the += operator with a raw pointer
+		//       as a second operand.
+		void operator+=(T* address)
+		{
+			database.push_back(*address);
+		}
+		
+		// TODO: Overload the += operator with a smart pointer
+		//       as a second operand.
+		void operator +=(std::shared_ptr<T> ptr)
+		{
+			database.push_back(*ptr);
+		}
+
+
+		void display(std::ostream& os) const {
+            os << std::fixed << std::setprecision(2);
+			for (auto& e : database)
+				os << e;
+        }
+	};
+
+    template<typename T>
+    std::ostream& operator<<(std::ostream& os, const DataBase<T>& db) {
+		db.display(os);
+		return os;
+	}
+}
+#endif
---
+++ WS08/w8p2/Utilities.h
@@ -0,0 +1,14 @@
+#ifndef SDDS_UTILITIES_H
+#define SDDS_UTILITIES_H
+// Workshop 8 - Smart Pointers
+
+#include "DataBase.h"
+#include "Profile.h"
+
+namespace sdds
+{
+	DataBase<Profile> excludeRaw(const DataBase<Profile>& allProfiles, const DataBase<Profile>& bannedProfiles);
+	DataBase<Profile> excludeSmart(const DataBase<Profile>& allProfiles, const DataBase<Profile>& bannedProfiles);
+}
+
+#endif
